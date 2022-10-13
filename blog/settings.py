0c6abe7cdab49f190af9586659ace286aeca1e61
Sohail Shams <sohail@jbcole.co.uk>
2022-10-13 10:33:51 +0100

    Added forgot password and change password functionality.

--- blog/settings.py
+++ blog/settings.py
@@ -145,3 +145,12 @@ LOGIN_REDIRECT_URL = 'home'
 
 # Redirect the user to login page on successfull logout
 LOGOUT_REDIRECT_URL = 'home'
+
+# Email send settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL =  str(os.environ.get('DEFAULT_FROM_EMAIL'))
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = str(os.environ.get('EMAIL_HOST_PASSWORD'))
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
