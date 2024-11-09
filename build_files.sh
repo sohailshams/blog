4f30c1b302f8bcd2fe052111e9ec558c6350b4a0
sohailshams <sohailshamas@yahoo.com>
2024-11-09 23:38:57 +0000

    Added correct version of Cython.

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 pip3 install --upgrade pip
 pip3 install python-dotenv
 pip3 install --upgrade pip setuptools wheel
-pip install Cython==0.29.36
+pip install Cython
 pip3 install -r requirements.txt
 python3 manage.py collectstatic
