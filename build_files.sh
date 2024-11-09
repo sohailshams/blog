75fc9404c9ff0a8a1ccb7c1ca0214e8d7abf9e74
sohailshams <sohailshamas@yahoo.com>
2024-11-09 23:39:57 +0000

    Corrected pip to pip3.

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 pip3 install --upgrade pip
 pip3 install python-dotenv
 pip3 install --upgrade pip setuptools wheel
-pip install Cython
+pip3 install Cython
 pip3 install -r requirements.txt
 python3 manage.py collectstatic
