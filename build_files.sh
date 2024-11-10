eb6da58ac71b83641ac4383e3879ba7ac918dfa0
sohailshams <sohailshamas@yahoo.com>
2024-11-10 10:51:38 +0000

    Added tailwind in build files.

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,7 @@ pip3 install python-dotenv
 python3 -m pip install Django
 pip3 install --upgrade pip setuptools wheel
 pip3 install dj-database-url
+pip3 install django-tailwind
 pip3 install Cython
 pip3 install PyYAML
 pip3 install -r requirements.txt
