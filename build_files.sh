8456f00fa9b32c3221d882fd6c3728797a36d808
sohailshams <sohailshamas@yahoo.com>
2024-11-10 10:49:49 +0000

    Added dj database in build files.

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,7 @@ pip3 install --upgrade pip
 pip3 install python-dotenv
 python3 -m pip install Django
 pip3 install --upgrade pip setuptools wheel
+pip3 install dj-database-url
 pip3 install Cython
 pip3 install PyYAML
 pip3 install -r requirements.txt
