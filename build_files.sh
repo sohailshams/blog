547244cf88e08d25731067968ff71e55541b5fbd
sohailshams <sohailshamas@yahoo.com>
2024-11-10 10:38:23 +0000

    Fixed pyyaml.

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,6 @@ pip3 install --upgrade pip
 pip3 install python-dotenv
 pip3 install --upgrade pip setuptools wheel
 pip3 install Cython
-pip3 install "PyYAML==6.0" --only-binary :all:
+pip3 install PyYAML
 pip3 install -r requirements.txt
 python3 manage.py collectstatic
